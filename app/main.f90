d832ad4f0a6c818ffdec03e94b7153031f3ecb96
FranciscoASanchez <89881287+FranciscoASanchez@users.noreply.github.com>
2023-11-24 15:15:04 -0300

    Add files via upload
    subí directo...

--- app/main.f90
+++ app/main.f90
@@ -1,89 +1,74 @@
 program main
-   use iso_fortran_env, only: pr => real64
-   use hard_spheres_mix, only: fugacity, alloc
-   implicit none
+  use iso_fortran_env, only: pr => real64
+  use hard_spheres_mix, only: fugacity, alloc
+  implicit none
 
-   integer :: nc
-   integer :: i, n_deriv, t_deriv, p_deriv, phase_type
+  integer   :: nc
+  integer   :: i, n_deriv, t_deriv, p_deriv, phase_type
 
-   integer :: funit_input
+  integer   :: funit_input, funit_output
+  character :: TAB = achar(9)   
 
-   call read_file("test.txt")
-   call run
-contains
-   subroutine read_file(filename)
-      use hard_spheres_mix, only: read_eos_parameters, alloc
-      integer :: funit_output
-      character(len=*), intent(in) :: filename
+  call read_file("test.txt")
+  call run
+ 
+ contains
+  subroutine read_file(filename)
+    use hard_spheres_mix, only: read_eos_parameters, alloc
+    character(len=*), intent(in) :: filename
 
-      open (newunit=funit_input, file=trim(filename))
-      read (funit_input, *) nc
-      call alloc(nc)
-      call read_eos_parameters(funit_input, funit_output, nc)
-   end subroutine
+    open (newunit=funit_input, file=trim(filename))
+    read (funit_input, *) model, nc
+    call alloc(nc)
+    call ReadParameters (funit_input, funit_output, model, NC)
+    close (unit=funit_input)
+    call run
+  end subroutine
 
-   subroutine run
-      ! Input variables
-      real(pr) :: p, t
-      real(pr) :: n(nc)
+  subroutine run
+ 
+    integer, parameter :: funit_data = 10, L = 2, V = 1
+ 
+    ! Input variables
+    real(pr) :: beta = 0, p, t
+    real(pr) :: n(nc)
 
-      ! Output variables
-      real(pr) :: v
-      real(pr) :: lnphi(nc), dlnphi_dn(nc, nc), dlnphi_dt(nc), dlnphi_dp(nc)
+    ! Output variables
+    real(pr) :: lnphi(nc), dlnphi_dn(nc, nc), dlnphi_dt(nc), dlnphi_dp(nc)
 
-      integer :: i, point
+    integer :: comp1, comp2, i, ndata, point
+    logical :: gas = .FALSE. , heavy = .FALSE., trace = .TRUE.
+    character(20) :: data_author(:), data_publ(:), data_year(:), dat_vol(:), data_pag(:)
 
-      read (funit_input, *) (n(i), i=1, nc), T, P
-      phase_type = 1
-      n_deriv = 2
-      t_deriv = 1
-      p_deriv = 1
-
-      do i=1,100000
-
-
-      call fugacity(nc, phase_type, &
-                    n_deriv, t_deriv, p_deriv, &
-                    T, P, n, &
-                    v, lnPhi, dlnPhi_dn, dlnPhi_dT, dlnPhi_dP &
-            )
-      end do
-
-      print "(G,2x,3(E14.5))", "lnPHI:     ", lnphi
-      print "(G,2x,3(E14.5))", "dlnPHI_dT: ", dlnphi_dt
-      print "(G,2x,3(E14.5))", "dlnPHI_dP: ", dlnphi_dp
-
-      do i=1,nc
-         print "(G,2x,I1,3(E14.5))", "dlnphi_dn:     ", i, dlnphi_dn(i, :)
-      end do
-   end subroutine
+    open (unit = funit_data, file="data.tsv")
+    read (funit_data, *) number_sets
+    i = 0
+    do while (iostat_i == 0)
+    
+      i = i + 1
+      write (itext, '(I2)') i
+      open (unit=funit_set, file="set_"//itext//".tsv")
+      read (funit_data, *, i) comp1, comp2
+      read (funit_data, *) data_authors(ndata), data_publ(ndata), data_year(ndata), data_vol(ndata), data_pag(ndata)
+      z(:) = 0
+      feed(comp1) = x(j)
+      trace(comp1) = .FALSE.
+      feed(comp2) = 1 - x(j)
+      trace(comp2) = .FALSE.
+        
+      do while (iostat_j == 0)
+        
+        read (funit_data, '(4G)', iostat=ios) T(j), P(j), x(j), y(j)
+        
+        if (iostat_j /= 0) exit
+        
+        call Flash (model, NC, calc_type, T, Pcalc, feed, trace, gas, heavy, beta, error_thermo, K, w, Z, var_spec, dXi_dS, lnPhi_out)
+        v_ig = Rgas*T/P
+        write (funit_set, *) Pexp, x, y, Pcalc, TAB, w(comp1,L), w(comp2,L), w(comp1,V), w(comp2,V), Z(L)*v_ig, Z(V)*v_IG
+          
+      enddo
+      close (unit=funit_set)
+  
+    enddo
+  end subroutine
 end program
-
-! program example
-!   use iso_fortran_env, only: pr => real64
-!
-!   implicit none
-!   integer, parameter                   :: inputFile = 1
-!
-!   integer                              :: i, n_deriv, NC, p_deriv, phase_type, t_deriv
-!   real(pr)                              :: P, T, V
-!   real(pr), dimension(:), allocatable   :: ac, b, dlnPhi_dP, dlnPhi_dT, m, n, lnPhi, Tc
-!   real(pr), dimension(:,:), allocatable :: dlnPhi_dn, k
-!
-!   open (unit = inputFile, file = "test.txt")
-!   read (inputFile, *) NC
-!   allocate (n(NC), lnPhi(NC), ac(NC), b(NC), m(NC), Tc(NC), k(NC,NC), dlnPhi_dP(NC), dlnPhi_dT(NC), dlnPhi_dn(NC,NC))
-!   call read_eos_parameters (inputFile, NC)
-!   read (inputFile, *) (n(i), i = 1, NC), T, P
-! !   call evaluate_mixture_parameters (NC, T, n, Tc, ac, b, m, k)
-!   phase_type = 1
-!   n_deriv = 2
-!   t_deriv = 1
-!   p_deriv = 1
-!   do i = 1, NC
-!   if (n(i) <= 0._pr) n(i) = 1D-20
-!   enddo
-!   call fugacity (NC, phase_type, n_deriv, t_deriv, p_deriv, T, P, n, v, lnPhi, dlnPhi_dn, dlnPhi_dT, dlnPhi_dP)
-!
-!   stop
-! endprogram example
